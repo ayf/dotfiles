02450b4faa497d2b99cc7982804d141e51369a61
Andrew Fong <andrew@Andrews-iMac-2.local>
2022-07-14 14:47:41 -0700

    add symlink

---
+++ install.sh
@@ -0,0 +1,7 @@
+# Install and Configure ZSH
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+    echo "source $PWD/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+
