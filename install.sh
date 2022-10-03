ed7babbf2941c5b6a03b211f1114a00097ff5387
Andrew Fong <andrew@mazeline.com>
2022-10-03 11:14:58 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,11 @@
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-    echo "source $PWD/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 
-ln -sf "$PWD/gitconfig" "$HOME/.gitconfig"
+echo "Installing ZSHRC"
 ln -sf "$PWD/zshrc" "$HOME/.zshrc"
+
+echo "source $PWD/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+
+echo "Installing GitConfig"
+ln -sf "$PWD/gitconfig" "$HOME/.gitconfig"
